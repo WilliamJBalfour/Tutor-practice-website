adc793fde0a159ccf0df40646801ada4e61381ca
William Balfour <willjbalfour@gmail.com>
2025-05-13 14:19:11 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Toggle mobile hamburger menu
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
   const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
@@ -5,4 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   toggleButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('active')
   })
+});
+
+// Change navbar background on scroll
+window.addEventListener('scroll', () => { //window => Global object that represents the browser window/tab
+  const navbar = document.querySelector('.navbar');
+
+  if(window.scrollY > 0) {
+    navbar.classList.add('navbar-scroll')
+  } else {
+    navbar.classList.remove('navbar-scroll')
+  }
 });
